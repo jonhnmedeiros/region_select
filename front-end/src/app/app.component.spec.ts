0f2257efc5d8078c6def952fd305a23d8159a361
Jonathan Medeiros <jonathan.info@gmail.com>
2025-01-30 14:21:40 -0300

    name project

--- front-end/src/app/app.component.spec.ts
+++ front-end/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fretefy-fullstack'`, () => {
+  it(`should have as title 'region_select'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fretefy-fullstack');
+    expect(app.title).toEqual('region_select');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fretefy-fullstack app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('region_select app is running!');
   });
 });
